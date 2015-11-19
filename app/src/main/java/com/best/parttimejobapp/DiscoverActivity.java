2db9c37d6f3544a14d2550c338d5a23518aa5db6
vonte <1123877821@qq.com>
2015-11-19 10:53:52 +0800

    add  详情页、分类页   from 朱文特

---
+++ app/src/main/java/com/best/parttimejobapp/DiscoverActivity.java
@@ -0,0 +1,15 @@
+package com.best.parttimejobapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vonte on 2015/11/18.
+ */
+public class DiscoverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover);
+    }
+}
